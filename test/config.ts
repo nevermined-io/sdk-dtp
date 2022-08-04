3284b220e42d5599cffa8fa42596fb7def830b05
Sami Mäkelä <mrsmkl@gmail.com>
2022-08-04 13:25:37 +0300

    testing in CI

--- test/config.ts
+++ test/config.ts
@@ -15,7 +15,8 @@ const configBase: Config = {
   faucetUri: 'http://localhost:3001',
   gatewayUri: 'http://localhost:8030',
   secretStoreUri: 'http://localhost:12001',
-  gatewayAddress: '0xe2DD09d719Da89e5a3D0F2549c7E24566e947260',
+  // gatewayAddress: '0xe2DD09d719Da89e5a3D0F2549c7E24566e947260',
+  gatewayAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0',
   marketplaceAuthToken: 'bogus',
   artifactsFolder: './artifacts',
   graphHttpUri: nograph

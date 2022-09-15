de8834fd0156bb84b23d2adccfce8162f682199c
Sami Mäkelä <mrsmkl@gmail.com>
2022-09-15 13:36:00 +0300

    remove recursive dependency

--- src/Dtp.ts
+++ src/Dtp.ts
@@ -4,14 +4,10 @@ import {
   InstantiableConfig
 } from '@nevermined-io/nevermined-sdk-js/dist/node/Instantiable.abstract'
 import { AccessProofTemplate } from './AccessProofTemplate'
-import { AccessProofCondition, AccessProofConditionExtra } from './AccessProofCondition'
-import { Account, MetaData, Nevermined } from '@nevermined-io/nevermined-sdk-js'
+import { AccessProofCondition } from './AccessProofCondition'
+import { Account } from '@nevermined-io/nevermined-sdk-js'
 import {
-  ServiceAccessProof,
-  ServiceCommon,
-  ServicePlugin,
   ServiceType,
-  ValidationParams
 } from '@nevermined-io/nevermined-sdk-js/dist/node/ddo/Service'
 import { makeKeyTransfer, KeyTransfer } from './KeyTransfer'
 import {
@@ -31,66 +27,6 @@ import { NFTAccessProofTemplate } from './NFTAccessProofTemplate'
 import { NFT721AccessProofTemplate } from './NFT721AccessProofTemplate'
 import { NFT721SalesWithAccessTemplate } from './NFT721SalesWithAccessTemplate'
 import { NFTSalesWithAccessTemplate } from './NFTSalesWithAccessTemplate'
-import { BaseTemplate } from '@nevermined-io/nevermined-sdk-js/dist/node/keeper/contracts/templates'
-import { decrypt } from './utils'
-
-export async function getAssetUrl(nevermined: Nevermined, did: string, index: number): Promise<{url: string, content_type: string}> {
-  // get url for DID
-  const asset = await nevermined.assets.resolve(did)
-  const service = asset.findServiceByType('metadata')
-  const file_attributes = service.attributes.main.files[index]
-  const content_type = file_attributes.contentType
-  const auth_method = asset.findServiceByType('authorization').service || 'RSAES-OAEP'
-  if (auth_method === 'RSAES-OAEP') {
-    const filelist = JSON.parse(await decrypt(service.attributes.encryptedFiles, 'PSK-RSA'))
-    // download url or what?
-    const url: string = filelist[index].url
-    return { url, content_type }
-  }
-  throw new Error('Bad authentication method')
-}
-
-export abstract class ProofTemplate<Params> extends BaseTemplate<Params> {
-  public dtp: Dtp
-  public async createService(
-    publisher: Account,
-    metadata: MetaData
-  ): Promise<ServiceCommon> {
-    const serviceAgreementTemplate = await this.getServiceAgreementTemplate()
-    return {
-      type: this.service(),
-      index: 10,
-      isDTP: true,
-      serviceEndpoint: this.nevermined.gateway.getServiceEndpoint(
-        this.service()
-      ),
-      templateId: this.getAddress(),
-      attributes: {
-        main: {
-          creator: publisher.getId(),
-          datePublished: metadata.main.datePublished,
-          name: this.name(),
-          timeout: 0,
-          _hash: metadata.additionalInformation!.poseidonHash,
-          _providerPub: [
-            metadata.additionalInformation!.providerKey!.x,
-            metadata.additionalInformation!.providerKey!.y
-          ]
-        },
-        serviceAgreementTemplate
-      }
-    } as ServiceCommon
-  }
-  public async extraGen(params: ValidationParams): Promise<any> {
-    const { url } = await getAssetUrl(this.nevermined, params.did, 0);
-    const data = Buffer.from(url, 'hex');
-    const extra : AccessProofConditionExtra = {
-      providerK: this.dtp.keytransfer.makeKey(process.env.PROVIDER_BABYJUB_SECRET),
-      data
-    };
-    return extra
-}
-}
 
 
 export class Dtp extends Instantiable {
